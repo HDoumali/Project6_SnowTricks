d028647b18a06f5f3d278474ec613149b7dd806a
HassanDoumali <doumalihassan4@yahoo.fr>
2018-06-11 17:35:33 +0200

    Realization of test on the addition og comments

---
+++ src/ST/UserBundle/Form/UserType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace ST\UserBundle\Form;
+
+use ST\UserBundle\Entity\User;
+use ST\AppBundle\Form\PictureType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('username', TextType::class)
+        ->add('firstname', TextType::class)
+        ->add('lastname', TextType::class)
+        ->add('picture', PictureType::class)
+        ->add('email', EmailType::class)
+        ->add('password', PasswordType::class)
+        ->add('Inscription', SubmitType::class);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ST\UserBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'st_userbundle_user';
+    }
+
+
+}
